7479d000f41b4af3b8a3533cfc2f4404f9dcf14f
unknown bxby <120569300+ishrak3232@users.noreply.github.com>
2023-01-11 18:13:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import colorama
-import argparse
 import requests, json
-import sys
-from sys import argv
 import os
 from colorama import Fore, Style, Back
 import time
+import ipinfo
+from ipinfo import *
+import webbrowser
+import geopy
+from geopy.geocoders import Nominatim
 
-
-
-
-
-
-#colours used
-red = '\033[31m'
-yellow = '\033[93m'
-lgreen = '\033[92m'
-clear = '\033[0m'
-bold = '\033[01m'
-cyan = '\033[96m'
-
-#banner of script
 colorama.init(autoreset=True)
 print('')
 print(f"{Fore.BLUE}             ╱╱╱╱╱╱╭╮")
@@ -35,37 +23,26 @@ print(f"{Fore.LIGHTCYAN_EX}                       ╱┃┃┃╰━╯┃")
 print(f"{Fore.LIGHTBLUE_EX}                       ╱┃┃┃╭━━╯     ")
 print(f"{Fore.LIGHTWHITE_EX}                      ╭┫┣┫┃")
 print(f'{Fore.WHITE}                      ╰━━┻╯')
-
 txt = f"{Fore.LIGHTCYAN_EX} put the ip "
 for letter in txt:
     colorama.init(autoreset=True) 
     print(letter, end="", flush=True)
     time.sleep(0.1)
-ip = input("  >>")
-
-api = "http://ip-api.com/json/"
-
-try: 
-        os.system("cls")
-        data = requests.get(api+ip).json()
-        sys.stdout.flush()
-        print ("[Victim]:", data['query'])
-        print ("[ISP]:", data['isp'])
-        print ("[Organisation]:", data['org'])
-        print ("[City]:", data['city'])
-        print ("[Region]:", data['region'])
-        print ("[Longitude]:", data['lon'])
-        print ("[Latitude]:", data['lat'])
-        print ("[Time zone]:", data['timezone'])
-        print ("[Zip code]:", data['zip'])
-
-
-except KeyboardInterrupt:
-        print ('Terminating, Bye')
-        sys.exit(0)
-except requests.exceptions.ConnectionError as e:
-        print (" check your internet connection!")
-        
-
-
-
+ip_address = input("  >>  ")
+handler = ipinfo.getHandler('a10aa7cbd78ac9')
+details = handler.getDetails(ip_address)
+geolocator = Nominatim(user_agent="unknown.ip")
+url = str(geolocator.reverse(details.loc))
+colorama.init(autoreset=True)
+print(f"{Fore.LIGHTRED_EX}latitude: "+details.latitude)
+print(f"{Fore.LIGHTRED_EX}longitude: "+details.longitude)
+print(f"{Fore.LIGHTRED_EX}country: "+details.country_name)
+print(f"{Fore.LIGHTRED_EX}city: "+details.city)
+print(f"{Fore.LIGHTRED_EX}region: "+details.region)
+print(f"{Fore.LIGHTRED_EX}postal: "+details.postal)
+print(f"{Fore.LIGHTRED_EX}timezone: "+details.timezone)
+print(f"{Fore.LIGHTRED_EX}org: "+details.org)
+print(f"{Fore.LIGHTRED_EX} address: "+url)
+last = input("press enter to exit....")
+if last == "":
+    os.system('exit')
